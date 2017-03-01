52e52bcafb5ff8a7b53b818b9538cc5b43ae8224
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2017-03-01 13:08:06 +0300

    Added request handlers.

--- app.js
+++ app.js
@@ -3,10 +3,43 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send("Hello World");
+const MODE_UNDEF = 'UNDEFINED';
+const MODE_WORK = 'WORK';
+const MODE_EVENT = 'EVENT';
+
+let detectorMode = MODE_UNDEF;
+
+
+///////////////////////////////////////////////////////////
+// Request handlers
+
+app.get('/set-work-mode', function (req, res) {
+  log('set work mode');
+  res.status(200).send('OK');
 });
 
-app.listen(3000, function(){
-    console.log('Example app listening on port 3000!');
+app.get('/reset-work-mode', function (req, res) {
+  log('reset work mode');
+  res.status(200).send('OK');
 });
+
+app.get('/set-event-mode', function (req, res) {
+  log('set event mode');
+  res.status(200).send('OK');
+});
+
+// Request handlers
+///////////////////////////////////////////////////////////
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+///////////////////////////////////////////////////////////
+// Utils
+
+function log(msg) {
+  console.log(msg);
+}
